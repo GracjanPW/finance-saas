38d7357e94c8c41a44bad695af7404b33f15b4e1
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-11-10 18:14:57 +0100

    categories CRUD and page

--- features/categories/components/new-category-sheet.tsx
+++ features/categories/components/new-category-sheet.tsx
@@ -5,24 +5,24 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { useNewAccount } from "../hooks/use-new-account";
-import { AccountForm } from "./account-form";
-import { insertAccountSchema } from "@/db/schema";
+import {  useNewCategory } from "../hooks/use-new-category";
+import { CategoryForm } from "./category-form";
+import {insertCategorySchema } from "@/db/schema";
 import { z } from "zod";
-import { useCreateAccount } from "../api/use-create-account";
+import { useCreateCategory } from "../api/use-create-category";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const formSchema = insertAccountSchema.pick({
+const formSchema = insertCategorySchema.pick({
   name:true,
 })
 
 type FormValues = z.input<typeof formSchema>;
 
 
-export const NewAccountSheet = () => {
-  const {isOpen, onClose} = useNewAccount();
+export const NewCategorySheet = () => {
+  const {isOpen, onClose} = useNewCategory();
 
-  const mutation = useCreateAccount();
+  const mutation = useCreateCategory();
 
   const onSubmit = (values:FormValues) =>{
     mutation.mutate(values, {
@@ -37,13 +37,13 @@ export const NewAccountSheet = () => {
       <SheetContent className="space-y-4">
         <SheetHeader>
           <SheetTitle>
-            New Account
+            New Category
           </SheetTitle>
           <SheetDescription>
-            Create a new account to track your transactions
+            Create a new category to organize your transaction.
           </SheetDescription>
         </SheetHeader>
-        <AccountForm 
+        <CategoryForm 
           onSubmit={onSubmit} 
           disabled={mutation.isPending}
           defaultValues={{
